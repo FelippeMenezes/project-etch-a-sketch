faba8f4cdba932813132ea4ddd09c7618217fb8f
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-02 11:23:18 -0300

    numberSquare function to draw 256 squares(16x16)

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+const numberOfSquares = 256;
+
 function sketchSquare() {
     const square = document.createElement('div');
     square.classList.add('square');
     document.body.append(square);
-}
+};
 
-sketchSquare();
+function numberSquare() {
+    for (let i = 0; i < numberOfSquares; i++) {
+        sketchSquare();
+    };
+};
 
-
+numberSquare();
 
 
